658b81ae27b28a4ce9770e7e451148cb345b29db
Ervandra Halim ⚡️ <ervandra.halim@gmail.com>
2026-03-06 07:20:49 +0700

    feat: Add react-modal and react-reveal dependencies with custom type definitions, and update package lock files.

---
+++ types/react-reveal.d.ts
@@ -0,0 +1,16 @@
+declare module 'react-reveal' {
+  export const Fade: any;
+  export const Flip: any;
+  export const Rotate: any;
+  export const Zoom: any;
+  export const Bounce: any;
+  export const Slide: any;
+  export const Roll: any;
+  export const LightSpeed: any;
+  const Reveal: any;
+  export default Reveal;
+}
+declare module 'react-reveal/*' {
+  const Reveal: any;
+  export default Reveal;
+}
